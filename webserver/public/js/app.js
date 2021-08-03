626c62026de6a81c35c0874d9db21776f2e61dc7
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-08-04 00:13:27 +0100

    fetching data

--- webserver/public/js/app.js
+++ webserver/public/js/app.js
@@ -1,1 +1,12 @@
 console.log("Client side javascript file is loaded");
+
+fetch("http://localhost:3000/weather?address=londofffn").then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log("Unable to find location! Try another search!");
+    } else {
+      console.log(data.forecastData);
+      console.log(data.location);
+    }
+  });
+});
